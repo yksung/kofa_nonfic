a8053706ea87aa0e7e45d0d94bde12449f071010
yksung <maria.yk.sung@gmail.com>
2016-08-05 05:45:10 +0900

    영상 및 장면 조회 기능 추

--- SceneEditor/src/main/java/kr/co/wisenut/monitor/dao/MonitorDao.java
+++
@@ -1,9 +0,0 @@
-package kr.co.wisenut.monitor.dao;
-
-import java.util.List;
-
-import kr.co.wisenut.monitor.model.Task;
-
-public interface MonitorDao {
-	public List<Task> getList() throws Exception;
-}
--- SceneEditor/src/main/java/kr/co/wisenut/scene/dao/SceneDao.java
+++
@@ -1,9 +0,0 @@
-package kr.co.wisenut.scene.dao;
-
-import java.util.List;
-
-import kr.co.wisenut.scene.model.Scene;
-
-public interface SceneDao {
-	public List<Scene> getList() throws Exception;
-}
